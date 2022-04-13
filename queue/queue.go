73d1bc9a20a529489f7a1702dcebde7b9694740a
Rafael <rafael_breno@outlook.com>
2022-04-13 02:00:00 -0300

    fix: Switch from Config to Internal and add logging

--- queue/queue.go
+++ queue/queue.go
@@ -1,7 +1,6 @@
 package queue
 
 import (
-	"github.com/rafaelbreno/nuveo-test/config"
 	"github.com/rafaelbreno/nuveo-test/internal"
 	rabbitmq "github.com/wagslane/go-rabbitmq"
 )
@@ -12,7 +11,7 @@ type (
 	Queue struct {
 		consumer  *rabbitmq.Consumer
 		publisher *rabbitmq.Publisher
-		cfg       *config.Config
+		internal  *internal.Internal
 	}
 )
 
@@ -21,16 +20,19 @@ type (
 func NewQueue(in *internal.Internal) (*Queue, error) {
 	c, err := rabbitmq.NewConsumer(in.Cfg.Queue.URL, rabbitmq.Config{})
 	if err != nil {
+		in.L.Error(err.Error())
 		return &Queue{}, err
 	}
 
 	p, err := rabbitmq.NewPublisher(in.Cfg.Queue.URL, rabbitmq.Config{})
 	if err != nil {
+		in.L.Error(err.Error())
 		return &Queue{}, err
 	}
 
 	return &Queue{
 		consumer:  &c,
 		publisher: p,
+		internal:  in,
 	}, nil
 }
