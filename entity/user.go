6be0e8634ff87b9b4c31d939093e43ba11a32c81
Rafael <rafael_breno@outlook.com>
2022-04-16 15:43:24 -0300

    feat: Add method to update fields and auto gen uuid

--- entity/user.go
+++ entity/user.go
@@ -6,10 +6,15 @@ type (
 	// User stores all values
 	// related to a client.
 	User struct {
-		UUID      string    `json:"uuid"`
+		UUID      string    `gorm:"type:uuid;default:gen_random_uuid()"`
 		Name      string    `json:"name"`
 		Address   string    `json:"address"`
 		CreatedAt time.Time `json:"created_at"`
 		UpdatedAt time.Time `json:"updated_at"`
 	}
 )
+
+func (u *User) UpdateFields(newUser User) {
+	u.Name = newUser.Name
+	u.Address = newUser.Address
+}
